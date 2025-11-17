5edb9b147ec4e5207a29a116de4be48e52bd57cc
Jeong Hoon Choi <csian7386@gmail.com>
2025-11-17 15:29:36 -0800

    [feat]: BaselineFCN.py & baseline_main.py upload

---
+++ baseline_main.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+"""baseline_main.py
+Description
+Flow based Network Anomaly Detection with Transformer based model
+
+Date
+Nov 10, 2025
+"""
+
+__author__ = "Jeong Hoon Choi"
+__version__ = "1.0.0"
+
+
+# Import #
+import os, sys
+sys.path.append("pylib/")
+import warnings
+warnings.filterwarnings("ignore")
+import logging
+from IPython.display import display
+
+from typing import Tuple, List
+import time
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+
+import tensorflow as tf
+from tensorflow.keras import mixed_precision
+from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
+
+from customize.NetFlowUtility import NetFlowLabelEncoder
+from customize.NetFlowMetrics import PrecisionMetric, RecallMetric, F1Macro, print_result
+from customize.BaselineFCN import BaseBatchLoader, BaselineFCN
+
+# dup2 #
+#fp = open(f"tmp/{datetime.now().strftime('%Y%m%d%H%M%S')}")
+#orig = os.dup2(fp.fileno(), sys.stdout.fileno())
+
+# Data Structures define - class #
+
+
+# Main Function Define
+def main(
+        data_path:str,
+        batch_size:int=64,
+        epochs:int=300,
+        hidden_sizes=[128, 64],
+        dropout:float=0.3,
+        class_weight:bool=False,
+        fout:str=None,
+        random_state:int=4444):
+    params = locals()
+
+    fp = sys.stdout
+    if fout:
+        fp = open(fout, 'a')
+    
+    print(tf.config.list_physical_devices("GPU"), file=fp)
+    # tf.debugging.set_log_device_placement=True
+    # mixed_precision.set_global_policy("mixed_float16")
+    
+    df = pd.read_csv(data_path)
+    le = NetFlowLabelEncoder()
+    df.iloc[:, -1] = le.fit_transform(df.iloc[:, -1])
+    
+    train_idx, temp_idx = train_test_split(
+        df.index,
+        test_size=0.3,
+        stratify=df.iloc[:, -1],
+        random_state=random_state
+    )
+    
+    val_idx, test_idx = train_test_split(
+        temp_idx,
+        test_size=0.5,
+        stratify=df.iloc[temp_idx, -1],
+        random_state=random_state
+    )
+
+    valid_idx = df.index[:]
+    train_mask = np.isin(valid_idx, train_idx)
+    val_mask = np.isin(valid_idx, val_idx)
+    test_mask = np.isin(valid_idx, test_idx)
+
+    array = df.to_numpy(dtype=np.float32)
+    X = array[:, :-1]
+    y = array[:, -1].astype(np.int32)
+    
+    train_ds = BaseBatchLoader(X, y, np.where(train_mask)[0], batch_size)
+    val_ds = BaseBatchLoader(X, y, np.where(val_mask)[0], batch_size)
+    test_ds = BaseBatchLoader(X, y, np.where(test_mask)[0], batch_size)
+    
+    model = BaselineFCN(
+        hidden_sizes=hidden_sizes,
+        dropout=dropout,
+        num_classes=le.size
+    )
+    
+    metrics = ["accuracy", F1Macro(), PrecisionMetric(), RecallMetric()] # ["accuracy"]
+    
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-4),
+        loss="sparse_categorical_crossentropy",
+        metrics=metrics,
+    )
+    
+    # model.build(input_shape=train_ds.element_spec[0].shape)
+
+    monitor = "val_loss"
+    mode = "auto"
+
+    if not class_weight:
+        monitor = "val_f1_macro"
+        mode = "max"
+    
+    early_stop = tf.keras.callbacks.EarlyStopping(
+        monitor=monitor,
+        mode=mode,
+        patience=5,
+        restore_best_weights=True
+    )
+    
+    lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(
+        monitor=monitor,
+        mode=mode,
+        factor=0.5,
+        patience=3,
+        min_lr=1e-6,
+        verbose=1
+    )
+
+    class_weights = None
+    
+    if class_weight:
+        classes = np.unique(y[np.where(train_mask)[0]])
+        class_weights = compute_class_weight("balanced",
+                                             classes=classes,
+                                             y=y[np.where(train_mask)[0]])
+        class_weights = dict(zip(classes, class_weights))
+    
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        steps_per_epoch=len(train_idx)//batch_size,
+        callbacks=[early_stop, lr_scheduler],
+        class_weight=class_weights,
+        verbose=1)
+
+    y_true = y[np.where(test_mask)[0]]
+    y_pred = model.predict(test_ds)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    print("=== Model Training Configuration ===", file=fp)
+    for key, value in params.items():
+        print(f"{key:<15}: {value}", file=fp)
+    print(file=fp)
+    
+    print_result(y_true, y_pred, le, fp)
+
+    if fout:
+        fp.close()
+
+# EP
+if __name__ == "__main__":
+    #sys.exit(main(sys.argv[1:]))
+    sys.exit(main(
+        data_path="data/nf-pre/NF-UNSW-NB15-v2-pre.csv",
+        epochs=300,
+        hidden_sizes=(128, 64),
+        dropout=0.3,
+        class_weight=True,
+        fout="tmp/metrics.txt",
+        random_state=4444))
+
+    
+#fp.close()
+#os.close(orig)
---
+++ pylib/customize/BaselineFCN.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""BaselineFCN.py
+Description
+Baseline FCN model
+
+Date
+Nov 17, 2025
+"""
+
+__author__ = "Jeong Hoon Choi"
+__version__ = "1.0.0"
+
+# Import #
+import warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Data Structures define - class #
+class BaselineFCN(tf.keras.Model):
+    def __init__(self, hidden_sizes=[128, 64], dropout=0.3, num_classes=10):
+        super().__init__()
+        self.hidden_layer = []
+        for h in hidden_sizes:
+            self.hidden_layer.append(layers.Dense(h, activation="relu"))
+            self.hidden_layer.append(layers.Dropout(dropout))
+
+        self.output_layer = layers.Dense(num_classes, activation="softmax")
+        
+    def call(self, X, training=False):
+        for layer in self.hidden_layer:
+            X = layer(X, training=training)
+        return self.output_layer(X)
+
+# Functions define #
+def BaseBatchLoader(X, y, indices, batch_size:int=32):
+    def generator():
+        batch_X, batch_y = [], []
+        for index in indices:
+            batch_X.append(X[index])
+            batch_y.append(y[index])
+            if len(batch_X) == batch_size:
+                yield np.array(batch_X, dtype=np.float32), np.array(batch_y, dtype=np.int32)
+                batch_X, batch_y = [], []
+
+        if batch_X:
+            yield np.array(batch_X, dtype=np.float32), np.array(batch_y, dtype=np.int32)
+            
+    dataset = tf.data.Dataset.from_generator(
+        generator,
+        output_signature=(
+            tf.TensorSpec(shape=(None, X.shape[1]), dtype=tf.float32),
+            tf.TensorSpec(shape=(None,), dtype=tf.int32)
+        )
+    ).prefetch(tf.data.AUTOTUNE)
+    return dataset
